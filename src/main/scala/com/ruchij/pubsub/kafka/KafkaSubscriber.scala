d315e40bbc4d40d84eb3a00ac692924adda1be4f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-05-09 22:48:40 +1000

    Updating the Kafka subscriber

--- src/main/scala/com/ruchij/pubsub/kafka/KafkaSubscriber.scala
+++ src/main/scala/com/ruchij/pubsub/kafka/KafkaSubscriber.scala
@@ -1,30 +1,25 @@
 package com.ruchij.pubsub.kafka
 
 import cats.effect.kernel.{Resource, Sync}
-import cats.~>
 import com.ruchij.config.KafkaConfiguration
 import com.ruchij.pubsub.Subscriber
 import com.ruchij.pubsub.models.CommittableRecord
 import com.ruchij.services.messages.models.Message
-import com.ruchij.types.FunctionKTypes.WrappedFuture
 import fs2.Stream
 import io.confluent.kafka.serializers.{AbstractKafkaSchemaSerDeConfig, KafkaAvroDeserializer, KafkaAvroDeserializerConfig}
 import org.apache.avro.specific.SpecificRecord
-import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, OffsetAndMetadata, OffsetCommitCallback}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, OffsetAndMetadata}
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
 
 import java.time.Duration
-import java.util
 import java.util.Properties
-import scala.concurrent.Promise
 import scala.jdk.CollectionConverters._
 
 class KafkaSubscriber[F[_]: Sync, A <: Message, B <: SpecificRecord](
   kafkaConfiguration: KafkaConfiguration,
   kafkaTopic: KafkaTopic[A, B]
-)(implicit futureUnwrapper: WrappedFuture[F, *] ~> F)
-    extends Subscriber[F, A] {
+) extends Subscriber[F, A] {
 
   private def consumerProperties(groupId: String): Properties =
     new Properties() {
@@ -61,28 +56,14 @@ class KafkaSubscriber[F[_]: Sync, A <: Message, B <: SpecificRecord](
           .map { consumerRecord =>
             CommittableRecord(
               kafkaTopic.fromSpecificRecord(consumerRecord.value()),
-              futureUnwrapper.apply {
-                Sync[F].blocking {
-                  val promise = Promise[Unit]()
-
-                  kafkaConsumer.commitAsync(
-                    Map(
-                      new TopicPartition(consumerRecord.topic(), consumerRecord.partition()) -> new OffsetAndMetadata(
-                        consumerRecord.offset()
-                      )
-                    ).asJava,
-                    new OffsetCommitCallback {
-                      override def onComplete(
-                        offsets: util.Map[TopicPartition, OffsetAndMetadata],
-                        exception: Exception
-                      ): Unit = {
-                        if (exception == null) promise.success((): Unit) else promise.failure(exception)
-                      }
-                    }
-                  )
-
-                  promise.future
-                }
+              Sync[F].blocking {
+                kafkaConsumer.commitSync(
+                  Map(
+                    new TopicPartition(consumerRecord.topic(), consumerRecord.partition()) -> new OffsetAndMetadata(
+                      consumerRecord.offset()
+                    )
+                  ).asJava
+                )
               }
             )
           }
--- src/test/scala/com/ruchij/pubsub/kafka/KafkaSpec.scala
+++ src/test/scala/com/ruchij/pubsub/kafka/KafkaSpec.scala
@@ -4,6 +4,7 @@ import cats.effect.{IO, Resource}
 import cats.implicits._
 import com.ruchij.avro.chat.OneToOneMessage
 import com.ruchij.external.embedded.EmbeddedExternalServiceProvider
+import com.ruchij.pubsub.models.CommittableRecord
 import com.ruchij.services.messages.models.UserMessage.OneToOne
 import com.ruchij.test.utils.IOUtils.runIO
 import com.ruchij.types.FunctionKTypes.ioFutureToIO
@@ -15,40 +16,58 @@ import org.scalatest.matchers.must.Matchers
 class KafkaSpec extends AnyFlatSpec with Matchers {
 
   "KafkaPublisher and KafkaSubscriber" should "be able to publish and subscribe to topics" in runIO {
-    new EmbeddedExternalServiceProvider[IO].kafkaConfiguration.flatMap {
-      kafkaConfiguration =>
-        KafkaPublisher.createProducer[IO](kafkaConfiguration)
+    new EmbeddedExternalServiceProvider[IO].kafkaConfiguration
+      .flatMap { kafkaConfiguration =>
+        KafkaPublisher
+          .createProducer[IO](kafkaConfiguration)
           .map(producer => new KafkaPublisher[IO, OneToOne, OneToOneMessage](producer, KafkaTopic.OneToOneMessageTopic))
           .product {
-            Resource.pure(new KafkaSubscriber[IO, OneToOne, OneToOneMessage](kafkaConfiguration, KafkaTopic.OneToOneMessageTopic))
+            Resource.pure(
+              new KafkaSubscriber[IO, OneToOne, OneToOneMessage](kafkaConfiguration, KafkaTopic.OneToOneMessageTopic)
+            )
           }
-    }.use {
-      case (kafkaPublisher, kafkaSubscriber) =>
-        kafkaSubscriber.subscribe("test-consumer").take(10).compile.toList.start
-          .productL {
-            kafkaPublisher.publish {
-              Stream.range[IO, Int](0, 10)
-                .evalMap { index =>
-                  JodaClock[IO].timestamp.map { timestamp =>
-                    OneToOne(index.toString, "my-sender", timestamp, "my-receiver", s"Hello World $index")
+      }
+      .use {
+        case (kafkaPublisher, kafkaSubscriber) =>
+          kafkaSubscriber
+            .subscribe("test-consumer")
+            .zip(Stream.range[IO, Int](0, 10))
+            .take(10)
+            .evalMap {
+              case (CommittableRecord(oneToOne, commit), index) =>
+                IO.delay {
+                    oneToOne.senderId mustBe "my-sender"
+                    oneToOne.receiverId mustBe "my-receiver"
+                    oneToOne.message mustBe s"Hello World $index"
+                    oneToOne.messageId mustBe index.toString
                   }
+                  .product(commit)
+                  .as(oneToOne)
+            }
+            .compile
+            .toList
+            .flatMap { oneToOneMessages =>
+              IO.delay {
+                oneToOneMessages.size mustBe 10
+                oneToOneMessages.map(_.sentAt.getMillis) mustBe sorted
+              }
+            }
+            .start
+            .productL {
+              kafkaPublisher
+                .publish {
+                  Stream
+                    .range[IO, Int](0, 10)
+                    .evalMap { index =>
+                      JodaClock[IO].timestamp.map { timestamp =>
+                        OneToOne(index.toString, "my-sender", timestamp, "my-receiver", s"Hello World $index")
+                      }
+                    }
                 }
+                .compile
+                .drain
             }
-              .compile
-              .drain
-          }
-          .flatMap(_.joinWithNever)
-          .flatMap { records =>
-            IO.delay {
-              records.size mustBe 10
-              all(records.map(_.data.senderId)) mustBe "my-sender"
-              all(records.map(_.data.receiverId)) mustBe "my-receiver"
-              records.map(_.data.messageId) mustBe Range(0, 10).map(_.toString)
-              records.map(_.data.message) mustBe Range(0, 10).map(index => s"Hello World $index")
-              records.map(_.data.sentAt.getMillis) mustBe sorted
-            }
-          }
-    }
+            .flatMap(_.joinWithNever)
+      }
   }
-
 }
