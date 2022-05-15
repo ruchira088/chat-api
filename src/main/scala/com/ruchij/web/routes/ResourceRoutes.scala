21ff3494d61ad0a4ccc377b0d21cadd3e23af477
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-05-15 17:04:10 +1000

    Updating the resource routes

--- src/main/scala/com/ruchij/web/routes/ResourceRoutes.scala
+++ src/main/scala/com/ruchij/web/routes/ResourceRoutes.scala
@@ -2,11 +2,11 @@ package com.ruchij.web.routes
 
 import cats.data.OptionT
 import cats.effect.kernel.Sync
-import org.http4s.Uri.Path.Segment
-import org.http4s.{HttpRoutes, StaticFile}
 import org.http4s.dsl.Http4sDsl
+import org.http4s.{HttpRoutes, StaticFile}
 
 object ResourceRoutes {
+  private val ServedFiles = Set("index.html", "script.js", "style.css")
 
   def apply[F[_]: Sync](implicit dsl: Http4sDsl[F]): HttpRoutes[F] = {
     import dsl._
@@ -15,8 +15,8 @@ object ResourceRoutes {
       case request @ GET -> Root =>
         StaticFile.fromResource("http/index.html", Some(request))
 
-      case request @ GET -> "resources" /: path =>
-        StaticFile.fromResource(Path(Vector(Segment("http"))).concat(path).renderString, Some(request))
+      case request @ GET -> Root / fileName if ServedFiles.contains(fileName) =>
+        StaticFile.fromResource(s"http/$fileName", Some(request))
 
       case _ => OptionT.none
     }
